6f75a14c3da19e467f80d4066e2bcd550f3e266a
xht0546 <xht0546@gmail.com>
2012-12-28 00:20:06 +0800

    进程数据结构建立

--- 10207170546_薛慧婷/进程调度算法/Process scheduling/Process scheduling/Form1.cs
+++ 10207170546_薛慧婷/进程调度算法/Process scheduling/Process scheduling/Form1.cs
@@ -15,5 +15,41 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void fcfs()
+        {
+            int sumztime;
+            int sumdqtime;
+            job[] j = { new job(1,2),new job(1,2),new job(1,2)};
+            for (int i = 0; i < 3; i++)
+            {
+                
+            }
+            
+        }
+    }
+
+    public class job
+    {
+        public int ctime;
+        public int stime;
+        public int ftime;
+        public int ztime;
+        public int dqtime;
+        public job(int a,int b) 
+        {
+            ctime = a;
+            stime = b;
+        }
+        public void  f()
+        {
+            ztime=ftime-ctime;
+            dqtime=ztime/stime;
+        }
     }
 }
